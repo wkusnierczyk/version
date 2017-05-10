614da7458dbd0a0b3361ef6225cbd9d974e7f7a1
Wacek Kusnierczyk <waclaw.kusnierczyk@gmail.com>
2017-05-11 01:35:27 +0200

    Code (#6)
    * Add version autoupdate script.
    * Add test script
    
    * Update travis file
    
    * Execute test script.

---
+++ bin/test.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# error codes
+export ERROR_SUCCESS=1
+export ERROR_FAILURE=2
+
+# error codes from version
+export ERROR_VERSION_STRING=1
+export ERROR_PART_FLAG=2
+
+# base directory of this project
+export BASE=$(cd $(dirname $BASH_SOURCE)/..; pwd -P)
+export VERSION="$BASE/src/version.sh"
+
+# test index
+export COUNTER=0
+
+# test that version correctly updates version string
+success() {
+	local VERSION="$1"
+	local OUTPUT="$2"
+	shift 2
+	let COUNTER+=1
+	printf "[%02d] $VERSION $@ ==> $OUTPUT... " $COUNTER
+	if [ "$($VERSION $@ 2>/dev/null)" = "$OUTPUT" ]; then
+	 	echo "Passed"
+	else
+		echo "Failed"
+		exit $ERROR_SUCCESS
+	fi
+}
+
+# test that version correctly fails on incorrect input
+failure() {
+	let COUNTER+=1
+	local VERSION="$1"
+	local ERROR="$2"
+	shift 2
+	printf "[%02d] $VERSION $@ fails with $ERROR... " $COUNTER
+	if [ "$($VERSION $@ >/dev/null 2>&1; echo $?)" -eq "$ERROR" ]; then
+		echo "Passed"
+	else
+		echo "Failed"
+		exit $ERROR_FAILURE
+	fi
+}
+
+success $VERSION 0.0.1 0.0.0
+
+success $VERSION 0.0.1 0.0.0 --build
+success $VERSION 0.1.0 0.0.0 --minor
+success $VERSION 1.0.0 0.0.0 --major
+
+success $VERSION 0.0.1 0.0.0 -b
+success $VERSION 0.1.0 0.0.0 -i
+success $VERSION 1.0.0 0.0.0 -a
+
+success $VERSION 0.0.1 --build 0.0.0
+success $VERSION 0.1.0 --minor 0.0.0
+success $VERSION 1.0.0 --major 0.0.0
+
+success $VERSION 0.0.1 -b 0.0.0
+success $VERSION 0.1.0 -i 0.0.0
+success $VERSION 1.0.0 -a 0.0.0
+
+success $VERSION 1.0.0 --build --major 0.0.0
+success $VERSION 1.0.0 -b --major 0.0.0
+success $VERSION 1.0.0 --build -a 0.0.0
+success $VERSION 1.0.0 -b -a 0.0.0
+
+failure $VERSION $ERROR_VERSION_STRING
+failure $VERSION $ERROR_VERSION_STRING ''
+failure $VERSION $ERROR_VERSION_STRING 0
+failure $VERSION $ERROR_VERSION_STRING .0
+failure $VERSION $ERROR_VERSION_STRING 0.
+failure $VERSION $ERROR_VERSION_STRING 0.0
+failure $VERSION $ERROR_VERSION_STRING .0.0
+failure $VERSION $ERROR_VERSION_STRING 0.0.
+failure $VERSION $ERROR_VERSION_STRING .0.0.0
+failure $VERSION $ERROR_VERSION_STRING 0.0.0.
+failure $VERSION $ERROR_VERSION_STRING foo.0.0
+failure $VERSION $ERROR_VERSION_STRING 0.foo.0
+failure $VERSION $ERROR_VERSION_STRING 0.0.foo
+
+failure $VERSION $ERROR_PART_FLAG 0.0.0 --foo
+failure $VERSION $ERROR_PART_FLAG 0.0.0 -f
+failure $VERSION $ERROR_PART_FLAG --foo 0.0.0
+failure $VERSION $ERROR_PART_FLAG -f 0.0.0
+
+. $VERSION
+
+success version 0.0.1 0.0.0 build
+success version 0.1.0 0.0.0 minor
+success version 1.0.0 0.0.0 major
+
+failure version $ERROR_VERSION_STRING
+failure version $ERROR_VERSION_STRING ''
+failure version $ERROR_VERSION_STRING 0
+failure version $ERROR_VERSION_STRING .0
+failure version $ERROR_VERSION_STRING 0.
+failure version $ERROR_VERSION_STRING 0.0
+failure version $ERROR_VERSION_STRING .0.0
+failure version $ERROR_VERSION_STRING 0.0.
+failure version $ERROR_VERSION_STRING .0.0.0
+failure version $ERROR_VERSION_STRING 0.0.0.
+failure version $ERROR_VERSION_STRING foo.0.0
+failure version $ERROR_VERSION_STRING 0.foo.0
+failure version $ERROR_VERSION_STRING 0.0.foo
+
+failure version $ERROR_PART_FLAG 0.0.0 foo
---
+++ src/version.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# requires extended (gnu) getopt with long options
+# execute to update a version string
+# source to import version function to sourcing shell
+
+# error codes
+export ERROR_VERSION_STRING=1
+export ERROR_PART_FLAG=2
+
+# given a version string in the form 'major.minor.build' and a flag naming one of the components
+# returns an updated version string with the named component increased by 1
+# if version string given in incorrect format, returns with code ERROR_VERSION_STRING
+# if no flag or wrong flag is given, returns with error code ERROR_PART_FLAG
+version() {
+	local CURRENT="$1"
+	local PART="$2"
+	grep -E '^[0-9]+[.][0-9]+[.][0-9]+$' >/dev/null <<< "$CURRENT" || {
+		echo "wrong or no version string: '$CURRENT'" >&2
+		return $ERROR_VERSION_STRING
+	}
+	local MAJOR=$(sed -E 's/^([0-9]+).*/\1/' <<< $CURRENT)
+	local MINOR=$(sed -E 's/^[0-9]+[.]([0-9]+).*/\1/' <<< $CURRENT)
+	local BUILD=$(sed -E 's/^[0-9]+[.][0-9][.]([0-9]+)$/\1/' <<< $CURRENT)
+	case "$PART" in
+		major) let MAJOR+=1;;
+		minor) let MINOR+=1;;
+		build) let BUILD+=1;;
+		*) echo "wrong or no part specification: '$PART'" >&2; return $ERROR_PART_FLAG;;
+	esac
+	echo "$MAJOR.$MINOR.$BUILD"
+}
+
+# if script sourced, return (the version function is available in the sourcing shell)
+if [ "$BASH_SOURCE" != "$0" ]; then
+	return
+fi
+
+# parse command line options
+TEMP=$(getopt -o aib --long major,minor,build -n $BASH_SOURCE -- "$@") || exit $ERROR_PART_FLAG
+eval set -- "$TEMP"
+
+# by default, increase the build number
+export PART='build'
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+    	-a|--major) PART='major'; shift;;
+    	-i|--minor) PART='minor'; shift;;
+    	-b|--build) PART='build'; shift;;
+    	--) shift; break;;
+    	*) echo "wrong command line argument: '$1'"; exit 1;;
+    esac
+done
+
+# execute
+version "$1" "$PART"
